eef22535fe4184ecdb26d4e404e988237d11f1b8
Vitor Tigre <vitorrtigre@gmail.com>
2023-10-26 15:10:48 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express()
 
 const caminhoBase = path.join (__dirname, "templates")
 
+
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendfile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendfile(`${caminhoBase}/index.html`)
 })
